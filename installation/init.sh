a3dc6d5e6a205d0d0b8c31495ca09c3ff99d54cb
galexis <a.w.gueguen@gmail.com>
2022-01-11 20:58:05 +0100

    new assets

--- installation/init.sh
+++ installation/init.sh
@@ -2,31 +2,31 @@
 
 echo "-- début du script --"
 
-echo "-- check venv installation --"
+echo -e "\n-- check venv installation --\n"
 sudo pip3 install virtualenv
+# vérifie que le module est bien installé
 
-echo "# création de l'environnement virtuel"
+echo -e "\n# création de l'environnement virtuel\n"
 python3 -m venv .venv
+# installation de l'environnement dans le dossier
 source .venv/bin/activate
+# active temporairement le venv
+
+echo -e "\n# installation via le fichier requirements.txt\n"
 pip3 install --upgrade pip
-pip3 install pycodestyle
-pip3 install autopep8
+pip3 install -r requirements.txt
 
-echo -e "\n# installation de flask"
-pip3 install flask
-
-
-echo -e "\n# création d'un fichier setting pour FLASK\n"
-pip3 install python-dotenv
+echo -e "\n# création du fichier .flaskenv"
 
 echo -e "FLASK_APP=run.py\nFLASK_ENV=development" > .flaskenv
+# définit un ensemble de variables, afin d'arrêter le développement et passer
+# en production changer la ligne 20.
 
-echo
-echo "-- fin du script --"
-echo
+echo -e "\n-- fin du script --\n"
+
 echo "pour lancer l'environnement :"
 echo "source .venv/bin/activate"
-echo
-echo "-- informations --"
+
+echo -e "\n-- informations --\n"
 echo "# la mention (.venv) doit être toujours visible dans le terminal lorsque vous travaillez sur ce projet"
 echo "# il suffit de lancer flask via 'flask run' l'ENV de dev étant déjà défini dans le fichier .flaskenv"
