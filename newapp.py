eb85201307bd5a068b5498c2be5c975a3df450b1
Alexis Watanabe <noxkuro@gmail.com>
2022-04-13 15:02:16 +0200

    POST comment added

--- newapp.py
+++ newapp.py
@@ -1,6 +1,6 @@
 import os
 from flask import (Flask, g, request, render_template, abort,
-                   redirect, send_from_directory, url_for)
+                   redirect, send_from_directory, url_for, flash)
 
 # methods ------------------------------------------------------------------- #
 import sqlite3
@@ -28,7 +28,16 @@ tabs_titles = {
                 'description', 'user_id', 'category_id'],
     'comment': ['id', 'timestamp', 'content', 'user_id', 'picture_id'],
     'tag': ['id', 'name'],
-    'tagtopicture': ['id', 'tag_id', 'picture_id']
+    'tagtopicture': ['id', 'tag_id', 'picture_id'],
+    'user': ['id', 'name']
+}
+
+post_tabs = {
+    'comment': ['content', 'user_id', 'picture_id'],
+    'picture': ['filename', 'description', 'user_id', 'category_id'],
+    'tag': ['name'],
+    'tagtopicture': ['tag_id', 'picture_id'],
+    'user': ['name']
 }
 
 pairs = {'category_id': 'category',
@@ -54,8 +63,11 @@ def query_db(query: str, args=(), one: bool = False):
     return ([rv[0]] if rv else None) if one else rv
 
 
-def update_db(query: str, args=()):
-    cur = get_db().execute(query, args)
+def update_db(table: str, args=()):
+    tabs = post_tabs[table]
+    sql_inject = ', '.join(list('?' * len(tabs)))
+    sql_query = f'INSERT INTO {table}({", ".join(tabs)}) VALUES ({sql_inject})'
+    cur = get_db().execute(sql_query, args)
     get_db().commit()
     return cur.lastrowid
 
@@ -72,21 +84,29 @@ def get_data(table: str, id=()):
     else:
         sql_request = f'SELECT * FROM {table}'
         rv = query_db(sql_request)
-
     clean_data = [{tabs[i]: j[i]
                    for i in range(len(tabs))} for j in rv]
     return clean_data
 
 
-def convert_data(rv: list):
+def clean_data(table, method='', args=()):
+    tabs = tabs_titles[table] if method == 'GET' else post_tabs[table]
+    return [{tabs[i]: j[i]
+             for i in range(len(tabs))} for j in args]
+
+
+def convert_data(rv: list, method='GET'):
     for i in rv:
         for j in i:
             if ('_id' in j) and (j != "picture_id"):
                 if str(i[j]).isalpha():
                     query = f'''SELECT id FROM {pairs[j]}
                             WHERE name = ?'''
-                    i[j] = query_db(query, (i[j],), one=True)[0][0]
-                elif str(i[j]).isnumeric():
+                    if query_db(query, (i[j],), one=True):
+                        i[j] = query_db(query, (i[j],), one=True)[0][0]
+                    elif j == 'user_id':
+                        i[j] = update_db('user', (i[j],))
+                elif str(i[j]).isnumeric() and method == 'GET':
                     query = f'''SELECT name FROM {pairs[j]}
                             WHERE id = ?'''
                     i[j] = query_db(query, (i[j],), one=True)[0][0]
@@ -118,6 +138,27 @@ def date_difference(date):
         return f'{value} year{"s" if value > 1 else ""} ago'
 
 
+def validator(request, conditions: list):
+    for i in conditions:
+        if i == 'picture':
+            filename = secure_filename(request.files['picture'].filename)
+            if filename == '':
+                return {"id": i, "e": 'missing picture'}
+            elif filename[filename.index('.'):] not in ALLOWED_EXTENSIONS:
+                return {"id": i, "e": 'invalid picture'}
+        elif not request.form[i]:
+            return {"id": i, "e": 'missing field'}
+        elif i == 'content' or i == 'description':
+            length = len(request.form.get('content')) if request.form.get(
+                'content') else len(request.form.get('description'))
+            if length > 300:
+                return {"id": i, "e": 'content too long'}
+        elif i == 'nickname' or i == 'name':
+            length = len(request.form.get('nickname')) if request.form.get(
+                'nickname') else len(request.form.get('name'))
+            if length > 20:
+                return {"id": i, "e": f'{i} too long'}
+
 # --------------------------------------------------------------------------- #
 # routes                                                                      #
 # --------------------------------------------------------------------------- #
@@ -125,7 +166,7 @@ def date_difference(date):
 # life cycle ---------------------------------------------------------------- #
 
 
-@app.before_request
+@ app.before_request
 def check_if_image_missing():
     rv = get_data('picture')
     sql_request = 'DELETE FROM picture WHERE filename = ?'
@@ -134,26 +175,26 @@ def check_if_image_missing():
             update_db(sql_request, (i["filename"],))
 
 
-@app.context_processor
+@ app.context_processor
 def menu_creator():
     rv = get_data('category')
     menu = list(map(lambda x: x['name'], rv))
     return dict(menu=menu)
 
 
-@app.errorhandler(404)
+@ app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
 
 
-@app.route('/uploads/<name>')
+@ app.route('/uploads/<name>')
 def secure_file(name):
     return send_from_directory(app.config['UPLOAD_FOLDER'], name)
 
 
 # index --------------------------------------------------------------------- #
 
-@app.route('/')
+@ app.route('/')
 def index():
     pictures = convert_data(get_data('picture'))
     tags = convert_data(get_data('tagtopicture'))
@@ -173,7 +214,7 @@ def index():
 
 # categories ---------------------------------------------------------------- #
 
-@app.route('/categories/<name>')
+@ app.route('/categories/<name>')
 def categories(name):
     rv = convert_data(get_data('picture'))
     pictures = list(filter(lambda x: (x['category_id'] == name), rv))
@@ -209,11 +250,26 @@ def show_picture(name):
     tags = convert_data(get_data('tagtopicture'))
 
     JINJA_DATA = {
+        'error': '',
         'picture': rv[0],
         'comments': sorted(list(
             filter(lambda x: (x['picture_id'] == id), comments)),
             key=lambda x: x['timestamp'], reverse=True),
         'tags': list(filter(lambda x: (x['picture_id'] == id), tags)),
     }
+
+    # POST ------------------------------------------------------------------ #
+    if request.method == 'POST':
+        error = validator(request, ['content', 'nickname'])
+        if error:
+            JINJA_DATA['error'] = error
+            return render_template('show.html', **JINJA_DATA)
+
+        rv = [tuple(request.form.values()) + (id,)]
+        res = convert_data(clean_data('comment', 'POST', rv))
+        args = tuple(res[0].values())
+        update_db('comment', args)
+
+        return redirect(url_for('show_picture', name=name))
 
     return render_template('show.html', **JINJA_DATA)
