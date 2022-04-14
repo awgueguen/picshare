fc483de1fe97338ac3ac9e5c591bfcbb5773c00a
Alexis Watanabe <noxkuro@gmail.com>
2022-04-14 16:06:02 +0200

    done

--- archive.old/init_db.py
+++ archive.old/init_db.py
@@ -85,85 +85,25 @@ def randomString():
 
 
 # users --------------------------------------------------------------------- #
-for name in ['Jean', 'Michou', 'Renaud', 'Marie', 'Louise']:
+for name in ['Jean', 'Michou', 'Renaud', 'Marie']:
     cursor.execute("INSERT INTO user (name) VALUES (?)", (name,))
 
 
 # categories ---------------------------------------------------------------- #
-for name in ["Graphisme", "Photographie", "Cinéma"]:
+for name in ["Cats", "Dogs", "People", "Backgrounds"]:
     cursor.execute("INSERT INTO category (name) VALUES (?)", (name,))
-
 
 # picture ------------------------------------------------------------------ #
 for data in [
-        ("carte-archi-1.jpg",
-         "Carte Archi", """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 1, 1),
-        ("deakins-1.jpg", "Deakins", """Lorem
-        ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere
-        sapien vel purus pretium, vitae lobortis turpis consequat.
-        In id auctor lectus.""", 2, 3),
-        ("deakins-2.jpg", "Deakins", """Lorem
-        ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 1, 3),
-        ("deakins-3.jpg", "Deakins", """Lorem
-        ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 3, 3),
-        ("forme-graphique-1.jpg",
-         "Forme Graphique", """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 4, 1),
-        ("illustration-livre-1.jpg",
-         "Illustration Livre", """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 1, 1),
-        ("japanese-poster-1.jpg",
-         "Japanese Poster", """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 2, 1),
-        ("japanese-poster-2.jpg",
-         "Japanese Poster", """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 1, 1),
-        ("leiter-1.jpg", "Leiter",
-         """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 3, 2),
-        ("lubezki-1.png", "Lubezki",
-         """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 3, 2),
-        ("lubezki-2.jpg", "Lubezki",
-         """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 1, 2),
-        ("photo-metro-1.jpg", "Photo Metro",
-         """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 2, 2),
-        ("photo-metro-2.jpg", "Photo Metro",
-         """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 1, 2),
-        ("poster-ntl-1.jpg",  "Poster NTL",
-         """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 3, 1),
-        ("poster-pyrexal-1.jpg",
-         "Poster Pyrexal", """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 1, 1),
-        ("webdesign-1.jpg",  "Webdesign",
-         """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 3, 1),
-        ("webdesign-2.jpg",  "Webdesign",
-         """Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Nulla posuere sapien vel purus pretium,
-        vitae lobortis turpis consequat. In id auctor lectus.""", 2, 1), ]:
+        ("cat-1.jpg",  "Cat", "Lorem ipsum dolor sit\
+         amet, consectetur adipiscing elit. Nulla sapien felis, ornare vitae\
+         urna quis, volutpat vestibulum sapien. Curabitur turpis lorem, \
+         viverra hendrerit dapibus nec, aliquet nec orci. Phasellus\
+         scelerisque risus sapien, non iaculis dolor condimentum ac.\
+         Nulla a enim elit efficitur.", 1, 1),
+        ("dog-1.jpg", "Dog", "Hello", 2,  2),
+        ("guy-1.jpg", "Guy", "Hello", 3, 3),
+        ("woman-1.jpg", "Woman", "Hello", 4, 3), ]:
     cursor.execute("""INSERT INTO picture
                         (filename,
                         name, description, user_id, category_id)
@@ -171,12 +111,12 @@ for data in [
 
 # comment ------------------------------------------------------------------ #
 comments = []
-for i in range(40):
+for i in range(20):
     comments.append(
         (f"""202{random.randint(1, 2)}-0{random.randint(1, 3)}-01
             {random.randint(10, 23)}:{random.randint(10, 46)}:00""",
          f'commentaire-\u2060{randomString()*2}', random.randint(1, 4),
-         random.randint(1, 17),))
+         random.randint(1, 4),))
 
 for data in comments:
     cursor.execute("""INSERT INTO comment
@@ -188,7 +128,7 @@ for data in comments:
 tags = ["photographie", "cinématographique", "poster", "web",
         "cinema", "random", "else"]
 
-for i in range(5):
+for i in range(10):
     tags += [f'photographer-\u2060{randomString()}']
 
 for tag in tags:
@@ -196,10 +136,10 @@ for tag in tags:
 
 
 # maintag ------------------------------------------------------------------- #
-for i in range(40):
+for i in range(20):
     cursor.execute(
         "INSERT INTO tagtopicture (tag_id, picture_id) VALUES (?, ?)",
-        (random.randint(1, 12), random.randint(1, 17),))
+        (random.randint(1, 10), random.randint(1, 4),))
 
 
 # We save our changes into the database file
